147369879104f64501da925b211852f066388001
Markzk2111 <106419589+Markzk2111@users.noreply.github.com>
2022-11-17 01:48:57 -0300

    Delete landingpageIphone directory

--- landingpageIphone/src/js/controlimg.js
+++
@@ -1,8 +0,0 @@
-function imgSlider(e){
-    document.querySelector('#phone').src = e;
-}
-/*Função de mudar a cor do circulo*/
-function circleChange(color){
-    const circle = document.querySelector('.circle');
-    circle.style.background = color;
-}
